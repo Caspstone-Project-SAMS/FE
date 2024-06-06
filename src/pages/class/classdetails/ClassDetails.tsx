c99d6fc7409677386f6e29fcb5c2c7c70b09008a
nguyenduc2710 <hongducnguyenho0@gmail.com>
2024-06-06 21:49:47 +0700

    fix merge

--- src/pages/class/classdetails/ClassDetails.tsx
+++ src/pages/class/classdetails/ClassDetails.tsx
@@ -1,25 +1,25 @@
 import { Content } from 'antd/es/layout/layout';
 import React from 'react';
-import '../Class.css';
-import { Button, Card, Space, Typography } from 'antd';
+import styles from '../Class.module.css';
+import { Button, Space, Typography } from 'antd';
 import ClassDetail from '../../../components/classdetails/ClassDetail';
 
 const ClassDetails: React.FC = () => {
   return (
-    <Content className="content">
-      <Space className="class-details-header">
+    <Content className={styles.content}>
+      <Space className={styles.classDetailsHeader}>
         <Space direction="vertical">
           <Typography.Title level={3}>Class</Typography.Title>
           <Space direction="horizontal">
             <text>Home / </text>
-            <text className="text-class">Class / SEP240_Room.302</text>
+            <text className={styles.textClass}>Class / SEP240_Room.302</text>
           </Space>
         </Space>
         <Space>
-          <Button className="btn-fap">
+          <Button className={styles.btnFap}>
             <Space direction='horizontal'>
-              <Typography.Text className='btn-text'>Check on FAP</Typography.Text>
-              <hr className='hr-line'></hr>
+              <Typography.Text className={styles.btnText}>Check on FAP</Typography.Text>
+              <hr className={styles.hrLine}></hr>
             </Space>
           </Button>
         </Space>
