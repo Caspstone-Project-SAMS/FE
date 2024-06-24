147bb65b16f85c01394906c9151c8a91a50d7aa3
nguyenduc2710 <hongducnguyenho0@gmail.com>
2024-06-24 13:59:45 +0700

    validate excel, show logs

--- src/components/excel/Excel.tsx
+++ src/components/excel/Excel.tsx
@@ -1,138 +1,132 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ExcelJS from 'exceljs'
-import { Button, message, Modal, Upload } from 'antd'
-import { UploadOutlined } from '@ant-design/icons'
-import { ValidateService } from './helpers/ValidateService'
+import { Button, message, Modal, Steps, Typography, Upload, UploadProps } from 'antd'
+import { CloudUploadOutlined, DeleteOutlined, FileTextOutlined } from '@ant-design/icons'
+import MessageCard from './messageCard/MessageCard'
+import { FileService } from './helpers/FileService'
+import { RcFile } from 'antd/es/upload'
+import styles from './index.module.less';
+import '../../assets/styles/styles.less'
 
 type validateFmt = {
-    result?: any,
-    errors: validateError[],
-}
-
-type validateError = {
-    type: 'warning' | 'error',
+    result?: any[];
+    errors: Message[];
+};
+type Message = {
     message: string
+    type: 'warning' | 'error',
 }
 
-// const props: UploadProps = {
-//     name: 'file',
-//     action: 'https://660d2bd96ddfa2943b33731c.mockapi.io/api/upload',
-//     headers: {
-//       authorization: 'authorization-text',
-//     },
-//     onChange(info) {
-//       if (info.file.status !== 'uploading') {
-//         console.log(info.file, info.fileList);
-//       }
-//       if (info.file.status === 'done') {
-//         message.success(`${info.file.name} file uploaded successfully`);
-//       } else if (info.file.status === 'error') {
-//         message.error(`${info.file.name} file upload failed.`);
-//       }
-//     },
-//   };
-
-const handleImportSemester = (excelFile, workbook: ExcelJS.Workbook) => {
-    const result: validateFmt = {
-        result: undefined,
-        errors: []
-    }
-
-    workbook.xlsx.load(excelFile)
-        .then(workbook => {
-            const worksheet = workbook.getWorksheet('Import-Semester')
-
-            const startRow = 4;
-            const endRow = 38;
-            const columns = [
-                {
-                    index: 'B',
-                    name: '#',
-                    isNull: false
-                },
-                {
-                    index: 'C',
-                    name: 'Mã Kỳ',
-                    isNull: false
-                },
-                {
-                    index: 'D',
-                    name: 'Tình trạng kỳ',
-                    isNull: false
-                },
-                {
-                    index: 'E',
-                    name: 'Ngày bắt đầu',
-                    isNull: false
-                },
-                {
-                    index: 'F',
-                    name: 'Ngày kết thúc',
-                    isNull: false
-                },
-                {
-                    index: 'G',
-                    name: 'Tạo bởi',
-                    isNull: false
-                },];
-
-            const sample = [];
-            for (let i = startRow; i <= endRow; i++) {
-                let isValidRecord = true;
-                const rowData: any = {};
-                columns.forEach((col) => {
-                    const cell = worksheet!.getCell(`${col.index}${i}`);
-                    //Check if null value allowed (no -> not noted)
-                    if ((cell.value === null) === col.isNull) {
-                        rowData[col.index] = cell.value;
-
-                        if (col.index === 'E') {
-                            const isValid = ValidateService.DateChecker('DD/MM/YYYY', cell.value);
-                            if (!isValid && isValid !== undefined) {
-                                result.errors.push({
-                                    type: 'error',
-                                    message: `Wrong date format at cell ${col.index}${i}`
-                                })
-                            }
-                        }
-                    } else {
-                        isValidRecord = false
-                        result.errors.push({
-                            type: 'warning',
-                            message: `Missing value at cell ${col.index}${i}`
-                        });
-                        return;
-                    }
-                });
-                if (isValidRecord) {
-                    sample.push(rowData);
-                }
-            }
-            console.log("after, ", sample);
-
-            const isGud = result.errors.length > 0 ? result.errors : 'Succeed, no error here'
-            console.log("Result here ", ...isGud);
-        })
+const props: UploadProps = {
+    progress: {
+        strokeColor: {
+            '0%': '#108ee9',
+            '100%': '#87d068',
+        },
+        strokeWidth: 3,
+        format: (percent) => percent && `${parseFloat(percent.toFixed(2))}%`,
+    },
 }
 
 const Excel = () => {
+    const [current, setCurrent] = useState(0);
     const [modalOpen, setModalOpen] = useState(false);
-    const wb = new ExcelJS.Workbook();
 
-    const handleExcel = (file) => {
+    //havent has api - not modelling data yet
+    const [excelResult, setExcelResult] = useState<validateFmt>();
+    const [errLogs, setErrLogs] = useState<Message[]>([]);
+    const [warningLogs, setWarningLogs] = useState<Message[]>([]);
+
+    const [onValidateExcel, setOnValidateExcel] = useState(false);
+    const [onValidateServer, setOnValidateServer] = useState(false);
+
+    function formatBytes(bytes: number) {
+        const kb = 1024;
+        const mb = kb * 1024;
+
+        if (bytes < kb) {
+            return `${bytes} Bytes`;
+        } else if (bytes < mb) {
+            const kilobytes = (bytes / kb).toFixed(2);
+            return `${kilobytes} KB`;
+        } else {
+            const megabytes = (bytes / mb).toFixed(2);
+            return `${megabytes} MB`;
+        }
+    }
+
+    const handleExcel = async (file: RcFile) => {
         try {
+            //15,728,640
+            console.log("file size: ", file.size)
+            if (file.size >= 15728640) {
+                message.error('File size exceed 15MB!', 1.5)
+                return false;
+            }
             const workbook = new ExcelJS.Workbook();
-            handleImportSemester(file, workbook);
+            const excelData = await FileService.handleImportSemester(file, workbook)
+            console.log("import data here", excelData);
+            setExcelResult(excelData)
+            setOnValidateExcel(true);
             return false
         } catch (error) {
             console.log("Error when receive excel file ", error);
             return false
         }
-
     }
 
+    const customItemRender = (originNode, file, fileList, actions) => {
+        return (
+            <div className={styles.fileItem}>
+                <div className={`flex item-center ${styles.itemBody}`}>
+                    <FileTextOutlined style={{ fontSize: 20, marginRight: '10px' }} />
+                    <div className={`flex-column`}>
+                        <span className={styles.fileName}>{file.name}</span>
+                        File size: {formatBytes(Number(file.size))}
+                    </div>
+                </div>
+                <div>
+                    <DeleteOutlined
+                        className={styles.fileIconDelete}
+                        onClick={() => {
+                            handleClearExcel();
+                            actions.remove(file)
+                        }} style={{ fontSize: 16 }}
+                    />
+                </div>
+            </div>
+        );
+    };
+
+    const handleClearExcel = () => {
+        setOnValidateExcel(false);
+        setExcelResult({
+            result: [],
+            errors: []
+        })
+        setErrLogs([])
+        setWarningLogs([])
+    }
+
+    useEffect(() => {
+        setErrLogs([]);
+        setWarningLogs([]);
+
+        excelResult?.errors.forEach(log => {
+            switch (log.type) {
+                case 'error':
+                    setErrLogs(prev => [...prev, log])
+                    break;
+                case 'warning':
+                    setWarningLogs(prev => [...prev, log])
+                    break;
+            }
+        })
+        // console.log("Changed ", excelResult);
+    }, [excelResult])
+
     return (
-        <div>
+        <div className={styles.excelValidateCtn}>
             <Button type="primary" onClick={() => setModalOpen(true)}>
                 Display a modal
             </Button>
@@ -143,13 +137,62 @@ const Excel = () => {
                 onOk={() => setModalOpen(false)}
                 onCancel={() => setModalOpen(false)}
             >
-                <Upload name='file'
-                    beforeUpload={(file) => handleExcel(file)}
-                    action={''}
-                //  onChange={(file) => handleExcel(file)}
+                <div
+                    style={{ minHeight: '70vh' }}
                 >
-                    <Button icon={<UploadOutlined />}>Click to Upload</Button>
-                </Upload>
+                    <Steps
+                        current={current}
+                        items={[
+                            {
+                                title: 'Validate Excel',
+                                description: 'hehe',
+                                // icon: onValidateExcel ? <LoadingOutlined /> : <FileExcelOutlined />
+                            },
+                            {
+                                title: 'Checking on server',
+                                description: 'hehe',
+                                // icon: onValidateServer ? <LoadingOutlined /> : <CloudServerOutlined />
+                            },
+                        ]}
+                    />
+                    <Upload.Dragger
+                        name='file'
+                        beforeUpload={(file) => handleExcel(file)}
+                        action={''}
+                        maxCount={1}
+                        {...props}
+                        itemRender={customItemRender}
+                    // accept={'.xlsx'}
+                    //  onChange={(file) => handleExcel(file)}
+                    >
+                        <p className="ant-upload-drag-icon">
+                            <CloudUploadOutlined />
+                        </p>
+                        <p className="ant-upload-text">
+                            <b style={{ color: '#2563EB' }}>Choose a file</b>{' '}
+                            or drop it here</p>
+                        <p className="ant-upload-hint">
+                            File receive: .xlsx <br />
+                            Max file size: 15MB
+                        </p>
+                    </Upload.Dragger>
+                    {
+                        onValidateExcel ? (
+                            <>
+                                <Typography.Text># Those record will be ignored, please consider again before continue</Typography.Text>
+                                {
+                                    (errLogs.length > 0) ? (<MessageCard props={errLogs} isSuccess={false} />) : ('')
+                                }
+                                {
+                                    (warningLogs.length > 0) ? (<MessageCard props={warningLogs} isSuccess={false} />) : ('')
+                                }
+                                {
+                                    (warningLogs.length === 0 && errLogs.length === 0) ? (<MessageCard props={[]} isSuccess={true} />) : ('')
+                                }
+                            </>
+                        ) : ('')
+                    }
+                </div>
             </Modal>
         </div>
     )
---
+++ src/components/excel/messageCard/MessageCard.tsx
@@ -0,0 +1,145 @@
+import React, { useState } from 'react'
+import styles from '../index.module.less'
+import '../../../assets/styles/styles.less';
+import { CheckCircleOutlined, DownOutlined, EnterOutlined, ExclamationCircleOutlined, InfoCircleOutlined } from '@ant-design/icons'
+import { AnimatePresence, motion } from 'framer-motion';
+
+type InfoLog = {
+    messages: string[],
+    type: 'success' | 'warning' | 'error'
+}
+
+type Message = {
+    message: string
+    type: 'warning' | 'error',
+}
+
+const MessageCard: React.FC<{ props: Message[], isSuccess: boolean }> = ({ props, isSuccess }) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const msgLength = props.length
+    const type = props[0]?.type || 'error';
+
+    const handleChangeLog = () => {
+        setIsOpen(!isOpen)
+    }
+
+    const msgType = {
+        success: {
+            message: 'Excel file good to go!',
+            icon: <CheckCircleOutlined size={20} />,
+            color: '#24D164',
+            bgColor: '#24D164'
+        },
+        warning: {
+            message: 'Excel file should adjust a few point',
+            icon: <InfoCircleOutlined size={20} />,
+            color: '#EAAD0D',
+            bgColor: '#FEF5DF'
+        },
+        error: {
+            message: 'Error has occurred',
+            icon: <ExclamationCircleOutlined size={20} />,
+            color: '#fd3d3e',
+            bgColor: '#ffefef'
+        }
+    }
+
+    return (
+        <div className={styles.msgCardCtn}>
+            {
+                isSuccess ? (
+                    <div className={styles.msgTitle}
+                        style={{
+                            // backgroundColor: msgType.success.bgColor,
+                            color: msgType.success.color,
+                            border: `1px solid ${msgType.success.color}`
+                        }}>
+                        <div className={'flex item-center'} >
+                            <span className={styles.msgIcon}>{msgType.success.icon}</span>
+                            Excel file good to go!
+                        </div>
+                    </div>
+                ) : (
+                    <div className={styles.msgTitle}
+                        style={{
+                            backgroundColor: msgType[type].bgColor,
+                            color: msgType[type].color,
+                            border: `1px solid ${msgType[type].color}`
+                        }}>
+                        <div className={'flex item-center'} >
+                            <span className={styles.msgIcon}>{msgType[type].icon}</span>
+                            {
+                                type === 'error' ? (
+                                    `${msgLength} errors has occurred`
+                                ) : (
+                                    `${msgLength} warning, adjust file for better result`
+                                )
+                            }
+                        </div>
+                        <motion.div
+                            whileInView={{
+                                rotate: isOpen ? (0) : (-90),
+                            }}
+                        >
+                            <DownOutlined onClick={() => handleChangeLog()} style={{ color: 'black' }} color='#FFFFFF' />
+                        </motion.div>
+                    </div>
+                )
+            }
+
+            <AnimatePresence>
+                {isOpen ? (
+                    <motion.div
+                        initial={{ opacity: 0.4, y: -30 }}
+                        exit={{ opacity: 0.4, y: -30 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        transition={{ duration: 0.65, ease: "easeInOut" }}
+                        className={styles.msgContents}
+                    >
+                        <MsgRecord
+                            messages={props.map(item => item.message)}
+                            type={type}
+                        />
+                    </motion.div>
+                ) : ('')
+                }
+            </AnimatePresence>
+
+        </div>
+    )
+}
+
+export default MessageCard;
+
+const MsgRecord: React.FC<InfoLog> = ({ messages, type }) => {
+    const msgType = {
+        success: {
+            color: '#24D164',
+            backgroundColor: ''
+        },
+        warning: {
+            color: '#EAAD0D',
+            backgroundColor: '#FFF9EA',
+            border: '1px solid #FBBF24'
+
+        },
+        error: {
+            color: '#FD3D3E',
+            backgroundColor: '#FFFAFA',
+            border: '1px solid #FD3D3E'
+        }
+    }
+    const recordStyle = msgType[type]
+
+    return (
+        <div className={styles.msgRecordCtn}
+            style={recordStyle}>
+            {messages.map((item, i) => (
+                <div key={`msgRecord-${i}`} className={styles.recordItem}>
+                    <EnterOutlined className={styles.msgRecordIcon} />
+                    <p className={styles.msgRecordTxt}>{item}</p>
+                </div>
+            ))}
+        </div>
+    )
+}
